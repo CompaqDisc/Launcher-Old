232c846e2238381293772689f3859ffba4db62f7
Herobrine2Nether <18bradan.wolbeck@gmail.com>
2015-07-17 18:14:23 -0600

    Initial Commit

---
+++ SchoolLauncherTS/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AppWindow
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.AppSelect = New System.Windows.Forms.ComboBox()
+        Me.AppIconBox = New System.Windows.Forms.PictureBox()
+        CType(Me.AppIconBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AppSelect
+        '
+        Me.AppSelect.FormattingEnabled = True
+        Me.AppSelect.Location = New System.Drawing.Point(12, 12)
+        Me.AppSelect.Name = "AppSelect"
+        Me.AppSelect.Size = New System.Drawing.Size(256, 21)
+        Me.AppSelect.TabIndex = 0
+        '
+        'AppIconBox
+        '
+        Me.AppIconBox.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.AppIconBox.Location = New System.Drawing.Point(12, 39)
+        Me.AppIconBox.Name = "AppIconBox"
+        Me.AppIconBox.Size = New System.Drawing.Size(256, 256)
+        Me.AppIconBox.TabIndex = 1
+        Me.AppIconBox.TabStop = False
+        '
+        'AppWindow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(280, 304)
+        Me.Controls.Add(Me.AppIconBox)
+        Me.Controls.Add(Me.AppSelect)
+        Me.Name = "AppWindow"
+        Me.Text = "App Launcher"
+        CType(Me.AppIconBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents AppSelect As System.Windows.Forms.ComboBox
+    Friend WithEvents AppIconBox As System.Windows.Forms.PictureBox
+
+End Class
---
+++ SchoolLauncherTS/Form1.vb
@@ -0,0 +1,68 @@
+﻿Public Class AppWindow
+    Dim AppConfig As String = ".\.lnk\lnk.csv"
+    Dim AppName() As String
+    Dim AppPath() As String
+    Dim AppIcon() As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadAppList()
+        LoadDefaultProg()
+    End Sub
+
+    Private Sub LoadAppList()
+        Try
+            Using ioReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(AppConfig)
+
+                ioReader.TextFieldType = FileIO.FieldType.Delimited
+                ioReader.SetDelimiters(",")
+
+                While Not ioReader.EndOfData
+
+                    Dim thisData As String() = ioReader.ReadFields()
+
+                    If AppName Is Nothing Then
+                        ReDim Preserve AppName(0)
+                        ReDim Preserve AppPath(0)
+                        ReDim Preserve AppIcon(0)
+                        AppName(0) = thisData(0)
+                        AppPath(0) = thisData(1)
+                        AppIcon(0) = thisData(2)
+
+                    Else
+                        ReDim Preserve AppName(AppName.Length)
+                        ReDim Preserve AppPath(AppPath.Length)
+                        ReDim Preserve AppIcon(AppIcon.Length)
+                        AppName((AppName.Length - 1)) = thisData(0)
+                        AppPath((AppPath.Length - 1)) = thisData(1)
+                        AppIcon((AppIcon.Length - 1)) = thisData(2)
+
+                    End If
+                End While
+            End Using
+            AppSelect.Items.AddRange(AppName)
+        Catch ex As Exception
+            MsgBox("Configuration File Not Found")
+            Me.Close()
+        End Try
+    End Sub
+
+    Private Sub AppSelect_SelectedIndexChanged(sender As Object, e As EventArgs) Handles AppSelect.SelectedIndexChanged
+        Try
+            AppIconBox.Image = Image.FromFile(AppIcon(AppSelect.SelectedIndex))
+        Catch ex As Exception
+            AppIconBox.Image = AppIconBox.ErrorImage
+        End Try
+    End Sub
+
+    Private Sub LoadDefaultProg()
+        AppSelect.SelectedIndex = 0
+    End Sub
+
+    Private Sub AppIconBox_Click(sender As Object, e As EventArgs) Handles AppIconBox.Click
+        Try
+            Process.Start(AppPath(AppSelect.SelectedIndex))
+        Catch ex As Exception
+            AppSelect.Text = "Could not load app..."
+        End Try
+    End Sub
+End Class
